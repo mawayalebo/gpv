de262131ff8f3abd299ad40e62d023a267a1cc46
unknown <mawayalebo@gmail.com>
2022-04-03 13:43:28 +0200

    styled header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,45 @@
 import Image from 'next/image'
+import Link from 'next/link';
 
 function Header() {
     return ( 
-        <div>
-            <div className="flex p-5 items-center">
-                <div className="relative">
-                    <Image src="/images/logo.png" alt="logo" width={80} height={80}/>
-                    <h1 className='text-xl sm:text-2xl md:text-4xl lg:text-6xl'>GPV</h1>
-                    <h1 className='text-xl sm:text-2xl md:text-4xl lg:text-6xl'>Group</h1>
+        <header className='px-4'>
+            {/* company Logo */}
+            <div className="flex items-center space-x-2 ">
+                <div className="bg-slate-200 flex items-center p-3 pt-10 rounded-b-3xl">
+                    <Image src="/images/logo.png" alt="logo" width={60} height={60}/>
+                    <h1 className='font-semibold text-3xl'>GPV</h1>
+                    <h1 className='font-bold text-3xl text-purple-700'>Group</h1>
+                </div>
+                {/* navigation */}
+                <div className='pl-5 pt-10'>
+                    <ul className='flex items-center space-x-5'>
+                        <li className='navBtn'>
+                            <Link href={"/"}>
+                                <div className=''>
+                                    <span className='text-slate-500'>Home</span>
+                                </div>
+                            </Link>
+                        </li>
+                        <li className='navBtn'>
+                            <Link href={"/"}>
+                                <div className=''>
+                                    <span className='text-slate-500'>Services</span>
+                                </div>
+                            </Link>
+                        </li>
+                        <li className='navBtn'>
+                            <Link href={"/"}>
+                                <div className=''>
+                                    <span className='text-slate-500'>About us</span>
+                                </div>
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
 
             </div>
-        </div>
+        </header>
      );
 }
 
